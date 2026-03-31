af0efcfaf1b4f4497b8f89cf9347cd730a2b1e81
z-Pearlina <pearlinaofficial@gmail.com>
2026-03-31 22:47:23 +0100

    add image domains for production

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,13 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   allowedDevOrigins: ["192.168.100.26"],
+  images: {
+    remotePatterns: [
+      { protocol: "https", hostname: "i.pinimg.com" },
+      { protocol: "https", hostname: "images.pexels.com" },
+      { protocol: "https", hostname: "images.unsplash.com" },
+    ],
+  },
 };
 
 export default nextConfig;
